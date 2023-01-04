3828a5dd422172fa1678f8eafb0c033ddabbadca
Hector Mtz <hec.gre81@gmail.com>
2023-01-03 19:03:23 -0600

    Modify magic numbers using define constant

--- chapter_1/for_reversed_fahr_to_celsius.c
+++ chapter_1/for_reversed_fahr_to_celsius.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
     printf("Celsius\tFahrenheit\n");
-    for (int i = 300; i >= 0; i -= 20) {
+    for (int i = UPPER; i >= LOWER; i -= STEP) {
         float calculation = (5.0/9) * (i - 32);
         printf("%3d\t%6.1f\n", i, calculation);
     }

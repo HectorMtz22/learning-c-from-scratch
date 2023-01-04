bc9820ec4726598a76280bde726fe7c4e8a609ff
Hector Mtz <hec.gre81@gmail.com>
2023-01-03 18:17:21 -0600

    Modifying for float numbers

--- chapter_1/fahr_to_celsius.c
+++ chapter_1/fahr_to_celsius.c
@@ -1,10 +1,12 @@
 # include <stdio.h>
 
-/* print Fahrenheit - Celsius table
+/* print Fahrenheit - Celsius table with float values
     for fahr = 0, 10, ..., 300; */
 
 int main() {
-    int fahr, celsius, lower, upper, step;
+    float fahr, celsius;
+    int lower, upper, step;
+
     lower = 0;
     upper = 300;
     step = 10;
@@ -12,8 +14,8 @@ int main() {
     fahr = lower;
     printf("Fahrenheit\tCelsius\n");
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t\t%d\n", fahr, celsius);
+        celsius = 5.0/ 9 * (fahr - 32); // We are doing division last to avoid truncation, i.e. 5/9 = 0 and 5.0/9 = 0.5555
+        printf("%3.0f\t\t%6.1f\n", fahr, celsius);
         fahr += step;
     }
 

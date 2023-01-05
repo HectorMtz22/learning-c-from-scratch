1fb7a9338e8d43afacd2daea3c740a10d0e64bbf
Hector Mtz <hec.gre81@gmail.com>
2023-01-04 18:46:27 -0600

    New way to do it, more compact

--- chapter_1/file_copying.c
+++ chapter_1/file_copying.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
+// 1st way
+// int main() {
+//     int c;
+
+//     c = getchar();
+//     while (c != EOF) {
+//         putchar(c);
+//         c = getchar();
+//     }
+//     return 0;
+// }
+
 int main() {
     int c;
 
-    c = getchar();
-    while (c != EOF) {
+    while ((c = getchar()) != EOF) {
         putchar(c);
-        c = getchar();
     }
     return 0;
 }

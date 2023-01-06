8f98bf52a993ba8a61d5799d056210ca52cce0c5
Hector Martinez <hec.gre81@gmail.com>
2023-01-05 22:50:04 -0600

    Added a note for "c - '0'"

--- chapter_1/count_digits_array.c
+++ chapter_1/count_digits_array.c
@@ -24,3 +24,15 @@ int main() {
     printf(", white space = %d, other = %d\n", nwhite, nother);
     return 0;
 }
+
+/*
+    * NOTE: in the code above, we use in line 12:
+    ? c - '0'
+    * This means that variable c stores the numeric value of the character i.e ASCII 
+    * If you want to use the number for the array, we must make this substraction.
+    * 0 = 48
+    * 1 = 49
+    * etc...
+    * if input is 0, we store 48
+    * Read more: https://www.quora.com/What-is-the-role-of-c-0-in-the-following-C-source-code
+*/

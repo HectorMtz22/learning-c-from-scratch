885515d16c7542524e33811de44ffdcf92687459
Hector Martinez <hec.gre81@gmail.com>
2023-01-11 21:34:52 -0600

    Experiment: each time I run this code I get different numbers on automatic var (i), it's a garbage value but Idk why it happens

---
+++ chapter_2/initial_vars.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// I initialized only j, so i has a garbage value.
+
+int main() {
+    int i;
+    int j = 0;
+
+    printf("int i = %d\nint j = %d\n", i, j);
+    return 0;
+}

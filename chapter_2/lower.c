b9fa573f7942ca336751fd5ffd0decccdd97be45
Hector Martinez <hec.gre81@gmail.com>
2023-01-12 21:56:40 -0600

    Convert uppercase string in a lowercase format and prints

---
+++ chapter_2/lower.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#define BUFFER 1000
+
+int lower(char c);
+
+int main() {
+    char line[BUFFER];
+
+    printf("Write a string in UPPERCASE: ");
+    scanf("%s", &line);
+
+    int n = strlen(line);
+
+    for (int i = 0; i < n; i++) {
+        line[i] = lower(line[i]);
+    }
+    
+    printf("\nThe same string in lowercase is: %s\n", line);
+    return 0;
+}
+
+int lower(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c + 'a' - 'A';
+    }
+    else return c;
+}

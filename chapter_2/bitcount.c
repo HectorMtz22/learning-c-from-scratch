ccf1cfcde65990e03565dbdad5ca00051a16bd15
Hector Martinez <hec.gre81@gmail.com>
2023-01-15 22:20:59 -0600

    Other implementation

--- chapter_2/bitcount.c
+++ chapter_2/bitcount.c
@@ -4,8 +4,11 @@ int bitcount (unsigned x) {
     int b = 0;
 
     while (x != 0) {
-        if (x & 01) b++;
-        x >>= 1;
+        // if (x & 01) b++;
+        // x >>= 1;
+        // Better implementation because we don´t have if
+        b++;
+        x &= (x - 1);
     }
 
     return b;

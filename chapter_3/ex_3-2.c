08a60d939212ad8e4ca91f8e1b7f125084d5f30f
Hector Martinez <hec.gre81@gmail.com>
2023-01-17 23:02:05 -0600

    Convert escape sequences

---
+++ chapter_3/ex_3-2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+void escape(char *s, char *t);
+
+int main() {
+    char string[] = "Hola, este es un mensaje random\twith some\t tabs and \nnewlines";
+    char new_string[1000];
+
+    escape(new_string, string);
+
+    printf("%s\n", new_string);
+    return 0;
+}
+
+void escape(char s[], char t[]) {
+    int length = strlen(t);
+    int i = 0;
+    for (int j = 0; j < length; j++) {
+        switch (t[i]) {
+        case '\n':
+            s[i++] = '\\';
+            s[i++] = 'n';
+            break;
+
+        case '\t':
+            s[i++] = '\\';
+            s[i++] = 't';
+            break;
+
+        default:
+            s[i++] = t[j];
+            break;
+        }
+    }
+    s[i] = '\0';
+}

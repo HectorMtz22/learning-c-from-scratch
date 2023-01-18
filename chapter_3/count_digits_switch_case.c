17f9277c05250203fa5730a92e1eb0e0a0de927f
Hector Martinez <hec.gre81@gmail.com>
2023-01-17 22:44:16 -0600

    Example for counting digits on input - Now with switch statement

---
+++ chapter_3/count_digits_switch_case.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define BASE 10 
+
+int main() {
+    int c, nwhite, nother, ndigit[BASE];
+    nwhite = nother = 0;
+    for (int i = 0; i < BASE; i++) {
+        ndigit[i] = 0;
+    }
+    while ((c = getchar()) != EOF) {
+        switch (c)
+        {
+        case '0': case '1': case '2': case '3': case '4': 
+        case '5': case '6': case '7': case '8': case '9':
+            ndigit[c - '0']++;
+            break;
+
+        case ' ':
+        case '\n':
+        case '\t':
+            nwhite++;
+            break;
+        
+        default:
+            nother++;
+            break;
+        }
+    }
+    printf("digits =");
+    for (int i = 0; i < BASE; i++) {
+        printf(" %d", ndigit[i]);
+    }
+    printf(", white spaces = %d, other = %d\n", nwhite, nother);
+    return 0;
+}
